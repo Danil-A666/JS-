07ecf3cc45c965fc8b4f44381ed3ff357704ecf4
Danil-A666 <154671419+Danil-A666@users.noreply.github.com>
2023-12-23 20:40:23 +0600

    Add files via upload
    Максимально простой js код для новичков.

---
+++ kyrs.js
@@ -0,0 +1,97 @@
+// console.log('Danil')
+
+// let a = 11
+// let b = 10
+//  a++
+// console.log(a + b)
+
+// let myName = 'Danil'
+// console.log(myName)
+
+// const objectA = {
+//     a: 10,
+//     b: true
+// }
+// const copyOfA = objectA
+// copyOfA.a = 20
+// copyOfA.c = 'Danil'
+// console.log(copyOfA)
+
+// const myObj = {
+//     myName: 'Dan'
+// }
+//     myObj.myName = 'Tom'
+// console.log(myObj)
+
+// const myObj = {
+//     age: 11,
+//     myName: 'Danil'
+// }
+//     myObj.myCity = 'Omsk'
+//     delete myObj.myName
+// console.log(myObj)
+
+// const name = 'Dan'
+// const postsQty = 23
+// const userProfile = {
+//     name,
+//     postsQty,
+//     hasSignedAgreement: false
+// }
+// console.log(userProfile)
+
+// const myObj = {
+//     myName: 'Dan',
+//     age: function () {
+//         console.log('LoL')
+//     }
+// }
+// myObj.age()
+
+// const person = {
+//     name: 'Dan',
+//     age: 11
+// }
+// const person2 = Object.assign({},person)
+// person2.age = 12
+// console.log(person)
+// console.log(person2)
+
+// let a = 5
+// let b = 3
+// function sum(a,b) {
+//     const c = a + b
+//     console.log(c)
+// }
+// sum(a,b)
+// a = 20
+// b = 11
+// sum(a,b)
+
+// function myFn(a,b) {
+//     let c
+//     a = a + 1
+//     c = a + b
+//     return c
+// }
+// console.log(myFn(10,5))
+
+// const personOne = {
+//     name: 'Dan',
+//     age: 11
+// }
+//
+// function fincreasePersonAge(person) {
+//     const updatedPerson = Object.assign({},person)
+//     updatedPerson.age += 1
+//     return updatedPerson
+// }
+//
+// const updatedPersonOne = fincreasePersonAge(personOne)
+// console.log(personOne.age)
+// console.log(updatedPersonOne.age)
+
+// function printMyName() {
+//     console.log('Dan')
+// }
+// setTimeout(printMyName,1000)
